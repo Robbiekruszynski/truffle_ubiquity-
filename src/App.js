396a64b1f73b848978ec0ca5a766242ddb83d503
robbie <robbiekruszynski@gmail.com>
2022-07-25 17:46:42 +0200

    rework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Metamask } from './MetaMask.js';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <button>Connect Wallet</button>
+        <h3>Wallet Adrress: XXXX</h3>
         <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
          Blockdaemon
         </p>
